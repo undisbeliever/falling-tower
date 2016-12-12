780ed5368ec306a57fb94b5eebdb5f6522b2eb4a
Marcus Rowe <undisbeliever@gmail.com>
2016-12-12 21:40:52 +1000

    refactor: Manually build MetaSprite data
    I have lost the script that I used to turn the png+utsi files into the
    MetaSprite data file (metasprite.gen). Therefore I have decided to
    recreate the MetaSprite data manually, so it can be more easily
    understood and edited.
    
    I also had to add more segments to the metasprite module and linker
    config in order to the new data to compile correctly.

--- src/resources/metasprites.s
+++ src/resources/metasprites.s
@@ -5,11 +5,89 @@
 
 .include "metasprite/metasprite.h"
 
-.setcpu "65816"
-
 .define METASPRITE_TILESET_BLOCK_0 "BANK3"
 
-.include "resources/metasprites/metasprite.gen"
+.define hFlip   $40
+.define vFlip   $80
+.define hvFlip  $C0
+.define small   $00
+.define large   $01
+
+.macro Object xPos, yPos, char, order, size, flip
+	.if .blank(flip)
+		Object xPos, yPos, char, order, size, 0
+	.else
+		.byte	MetaSprite::POSITION_OFFSET + xPos
+		.byte	MetaSprite::POSITION_OFFSET + yPos
+		.byte	char
+		.byte	(order << 4) | size | flip
+	.endif
+.endmacro
+
+.macro TileHitbox xPos, yPos, width, height
+		.byte	MetaSprite::POSITION_OFFSET + xPos
+		.byte	MetaSprite::POSITION_OFFSET + yPos
+		.byte	width
+		.byte	height
+.endmacro
+
+.segment METASPRITE_FRAMESET_LIST_BLOCK
+.assert .loword(*) <> 0, error, "Cannot start data with addr=0"
+.export MetaSpriteFrameSetTable: far
+MetaSpriteFrameSetTable:
+
+.segment METASPRITE_FRAMESET_DATA_BLOCK
+.assert .loword(*) <> 0, error, "Cannot start data with addr=0"
+.exportzp MetaSprite__FrameSet_Bank = .bankbyte(*)
+
+.segment METASPRITE_FRAME_LIST_BLOCK
+.assert .loword(*) <> 0, error, "Cannot start data with addr=0"
+.export MetaSprite__Frame_List: far
+MetaSprite__Frame_List:
+
+.segment METASPRITE_FRAME_DATA_BLOCK
+.assert .loword(*) <> 0, error, "Cannot start data with addr=0"
+.exportzp MetaSprite__Frame_Bank = .bankbyte(*)
+
+.segment METASPRITE_TILESET_BLOCK
+.assert .loword(*) <> 0, error, "Cannot start data with addr=0"
+.exportzp MetaSprite__Tileset_Bank = .bankbyte(*)
+
+.segment METASPRITE_DMA_TABLE_BLOCK
+.assert .loword(*) <> 0, error, "Cannot start data with addr=0"
+.exportzp MetaSprite__DmaTable_Bank = .bankbyte(*)
+
+.segment METASPRITE_FRAME_OBJECTS_BLOCK
+.assert .loword(*) <> 0, error, "Cannot start data with addr=0"
+.exportzp MetaSprite__FrameObjectsList_Bank = .bankbyte(*)
+
+.segment METASPRITE_TILE_COLLISION_HITBOXES_BLOCK
+.assert .loword(*) <> 0, error, "Cannot start data with addr=0"
+.exportzp MetaSprite__TileCollisionHitbox_Bank = .bankbyte(*)
+
+.segment METASPRITE_ENTITY_COLLISION_HITBOXES_BLOCK
+.assert .loword(*) <> 0, error, "Cannot start data with addr=0"
+.exportzp MetaSprite__EntityCollisionHitboxes_Bank = .bankbyte(*)
+
+.segment METASPRITE_ACTION_POINT_BLOCK
+.assert .loword(*) <> 0, error, "Cannot start data with addr=0"
+.exportzp MetaSprite__ActionPoints_Bank = .bankbyte(*)
+
+
+
+.include "resources/metasprites/player.inc"
+.include "resources/metasprites/platforms.inc"
+
+
+
+.segment METASPRITE_FRAMESET_LIST_BLOCK
+.export MetaSpriteFrameSetTable_end: far
+MetaSpriteFrameSetTable_end:
+.export MetaSprite__FrameSet_Count = (MetaSpriteFrameSetTable_end - MetaSpriteFrameSetTable) / 2
+
+.segment METASPRITE_FRAME_LIST_BLOCK
+.export MetaSprite__Frame_Count = (MetaSprite__Frame_List_end - MetaSprite__Frame_List) / 2
+MetaSprite__Frame_List_end:
 
 ; vim: set ft=asm:
 
